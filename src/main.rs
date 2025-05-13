70be73d3b1d9615716eb85c67dc155b86e2e5ee6
Emil Björnlinger <15017341+emilbjornlinger@users.noreply.github.com>
2025-05-13 11:44:16 +0200

    File input without MacroString crate

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
 use taint_macros;
+use std::fs;
 
 mod taint;
 
 fn main() {
     let my_taint = taint_macros::source!("user input".to_string());
     let my_num_taint = taint_macros::source!(3);
+    let my_file_taint = taint_macros::source!(fs::read_to_string("input.txt"));
 
     println!("{}", my_taint.sanitize());
 
     println!("5 + sanitized input: {}", 5 + my_num_taint.sanitize());
+
+    println!("file_taint input: {}", my_file_taint.sanitize().unwrap());
 }
